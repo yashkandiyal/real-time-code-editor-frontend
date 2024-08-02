3925c0cfca96d0252b21cf1416377a59437ddc05
Yashkandiyal <126400221+yashkandiyal@users.noreply.github.com>
2024-08-02 20:35:59 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ if (!PUBLISHABLE_KEY) {
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/" signInFallbackRedirectUrl="/">
         <App />
       </ClerkProvider>
     </BrowserRouter>
